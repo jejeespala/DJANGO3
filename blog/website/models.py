089ee34104edb3c6524fd9e414067d88f52b2bcc
Jerônimo Espala <jeronimo_espala@hotmail.com>
2021-03-20 08:21:04 -0300

    Lendo Post no Banco

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     sub_title = models.CharField(max_length=30)
     content = models.TextField()
+    categorias = models.CharField(
+        max_length=2,
+        choices=Categories.choices,
+        default=Categories.GR
+    )
+
+    approved = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return self.title
+
     
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,8 +1,17 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 def hello_blog(request):
     lista = ['django', 'python', 'git', 'linux', 'ngix']
-    data = {'name': 'Curso de Django', 'lista_tecnologias': lista}
+
+    list_posts = Post.objects.all()
+
+    data = {'name': 'Curso de Django',
+     'lista_tecnologias': lista,
+     'posts': list_posts}
+
+    
     return render(request,'index.html', data)
